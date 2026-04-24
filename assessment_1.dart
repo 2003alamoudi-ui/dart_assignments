19ad08aa61c6e6890d7af740bf017643629d6e5a
2003alamoudi-ui <2003alamoudi@gmail.com>
2026-04-24 22:18:38 +0300

    Add initial implementation of assessment_1.dart
    Add assesssment 1

---
+++ assessment_1.dart
@@ -0,0 +1,23 @@
+// Assignment 1 - Variables & Keywords
+
+late String userProfile;
+
+void main() {
+
+  userProfile = "Mobile Developer";
+
+  const double appVersion = 1.0;
+
+  final loginTime = DateTime.now();
+
+  final now = DateTime.now();
+
+  print(userProfile);
+
+  print(appVersion);
+
+  print(loginTime);
+
+  print(now);
+
+}
