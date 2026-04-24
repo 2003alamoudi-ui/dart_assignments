d779ace999e3c01aad754c3f2b502a40e50c69e4
2003alamoudi-ui <2003alamoudi@gmail.com>
2026-04-24 22:40:02 +0300

    Add main function with variable declarations
    Implement main function to demonstrate variable types.

---
+++ assessment_4.dart
@@ -0,0 +1,21 @@
+// Assignment 4 - Types
+
+void main() {
+
+  double temperature = 36.5;
+
+  int passengers = 20;
+
+  String address = "Cairo, Egypt";
+
+  bool isDarkMode = true;
+
+  print(temperature);
+
+  print(passengers);
+
+  print(address);
+
+  print(isDarkMode);
+
+}
